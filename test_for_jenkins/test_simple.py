6d7bea1807f3265de015c99f39c3899dc5e68e0d
VadymShurkhal <vadimshurhal@gmail.com>
2023-08-16 14:09:58 +0300

    add allure to test for jenkins

--- test_for_jenkins/test_simple.py
+++ test_for_jenkins/test_simple.py
@@ -1,8 +1,20 @@
+import allure
+
+
+@allure.story('My first story')
+class TestAllure:
+
 class TestClass:
 
+    @allure.description('My test description test_upper')
+    @allure.title('Test assert #1')
+    @allure.severity("Low")
     def test_upper(self):
         assert 'foo'.upper() == 'FOO'
 
+    @allure.description('My test description test_isupper')
+    @allure.title('Test assert #2')
+    @allure.severity("Height")
     def test_isupper(self):
         assert 'FOO'.isupper()
 
