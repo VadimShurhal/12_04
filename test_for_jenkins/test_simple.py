02a0c867fbd7f10d520eeb0b49ca0e3c17b6e6d0
VadymShurkhal <vadimshurhal@gmail.com>
2023-08-16 13:53:21 +0300

    change test for jenkins

--- test_for_jenkins/test_simple.py
+++ test_for_jenkins/test_simple.py
@@ -1,13 +1,8 @@
 class TestClass:
 
-    def setup(self):
-        print('Setup method')
-
-    def teardown(self):
-        print('teardown method')
-
     def test_upper(self):
         assert 'foo'.upper() == 'FOO'
 
     def test_isupper(self):
         assert 'FOO'.isupper()
+
